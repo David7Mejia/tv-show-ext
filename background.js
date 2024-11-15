715f7c0242fe1da5aa1773d3e8c4e3d400d19e9b
David Mejia <david7mejia@gmail.com>
2024-11-15 16:15:00 -0500

    context menus, parent context menus

--- background.js
+++ background.js
@@ -1,5 +1,29 @@
 chrome.runtime.onInstalled.addListener(details => {
   console.log(details);
+
+  chrome.contextMenus.create({
+    title: "Search for TV Show",
+    id: "contextMenu1",
+    contexts: ["page", "selection"],
+  });
+
+  chrome.contextMenus.onClicked.addListener((event, tab) => {
+    console.log(event);
+  });
+
+  chrome.contextMenus.create({
+    title: "Search for TV Show",
+    id: "contextMenu12",
+    parentId: "contextMenu1",
+    contexts: ["page", "selection"],
+  });
+
+  chrome.contextMenus.create({
+    title: "Search for TV Show",
+    id: "contextMenu3",
+    parentId: "contextMenu1",
+    contexts: ["page", "selection"],
+  });
 });
 
 console.log("background script running");
