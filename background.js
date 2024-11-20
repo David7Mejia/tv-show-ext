561f29c68bf57e09cea93c350534b01fc00861f8
David Mejia <david7mejia@gmail.com>
2024-11-19 19:10:26 -0500

    adding content script

--- background.js
+++ background.js
@@ -7,23 +7,33 @@ chrome.runtime.onInstalled.addListener(details => {
     contexts: ["page", "selection"],
   });
 
-  chrome.contextMenus.onClicked.addListener((event, tab) => {
-    console.log(event);
-  });
+  //  Search on google
+  // chrome.contextMenus.onClicked.addListener((event, tab) => {
+  //   chrome.search.query({
+  //     disposition: "NEW_TAB",
+  //     text: `imdb ${event.selectionText}`,
+  //   });
+  // });
+  // __________________________________
 
-  chrome.contextMenus.create({
-    title: "Search for TV Show",
-    id: "contextMenu12",
-    parentId: "contextMenu1",
-    contexts: ["page", "selection"],
+  chrome.contextMenus.onClicked.addListener(event => {
+    chrome.tabs.create({
+      url: `https://www.imdb.com/find?q=${event.selectionText}`,
+    });
   });
+  // chrome.contextMenus.create({
+  //   title: "Search for TV Show",
+  //   id: "contextMenu12",
+  //   parentId: "contextMenu1",
+  //   contexts: ["page", "selection"],
+  // });
 
-  chrome.contextMenus.create({
-    title: "Search for TV Show",
-    id: "contextMenu3",
-    parentId: "contextMenu1",
-    contexts: ["page", "selection"],
-  });
+  // chrome.contextMenus.create({
+  //   title: "Search for TV Show",
+  //   id: "contextMenu3",
+  //   parentId: "contextMenu1",
+  //   contexts: ["page", "selection"],
+  // });
 });
 
 console.log("background script running");
